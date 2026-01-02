436c98ec3f310cefc8d4f92d503fcfdd4e8215f7
mgrunnill <Martin.Grunnill@phac-aspc.gc.ca>
2026-01-02 17:28:28 -0500

    Moved test to be a class.

--- tests/test_generic_workflow.py
+++ tests/test_generic_workflow.py
@@ -5,13 +5,9 @@ from papermill.iorw import read_yaml_file
 from pathlib import Path
 from papermill import execute_notebook
 import os
-from _test_parts import _add_tmp_path_to_params_yml_save, _test_running_of_workflow
+from _test_parts import WorkFlowVariationTest
 
-
-
-def test_full_workflow(tmp_path, subtests):
-    new_parameters_path = _add_tmp_path_to_params_yml_save(
-        tmp_path,
-        'parameters/locally_run_examples/Generic_full.yml')
-    worklflow = 'Generic'
-    _test_running_of_workflow(tmp_path, subtests, worklflow, new_parameters_path)
+class TestGenericFull(WorkFlowVariationTest):
+    parameter_path = 'parameters/locally_run_examples/Generic_full.yml'
+    workflow = 'Generic'
+    variation = 'full'
